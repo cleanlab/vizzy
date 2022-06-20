c0ef853624b0e35e2e166eaaa71000db025dd9e9
calebchiam <calebchiam@gmail.com>
2022-06-19 18:33:44 -0700

    style label button

--- src/components/dataset/ImageWithLabel.tsx
+++ src/components/dataset/ImageWithLabel.tsx
@@ -1,11 +1,40 @@
 import React from 'react'
-import { Box, Image, ImageProps } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Flex,
+  Image,
+  ImageProps,
+  Menu,
+  Portal,
+  Select,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react'
 import { ImageWithLabelProps } from './types'
+import { BiChevronDown } from 'react-icons/bi'
 
 const ImageWithLabel = (props: ImageWithLabelProps) => {
   const { givenLabel, labelOptions } = props
   return (
-    <Box>
+    <Box position={'relative'}>
+      <Menu>
+        <Button
+          // colorScheme={useColorModeValue('blackAlpha', 'blackAlpha')}
+          backgroundColor={'white'}
+          opacity={'50%'}
+          color={'black'}
+          size={'xs'}
+          bottom={'0px'}
+          right={'0px'}
+          position={'absolute'}
+          maxWidth={'100%'}
+          rightIcon={<BiChevronDown />}
+        >
+          <Text noOfLines={1}>{givenLabel}</Text>
+        </Button>
+      </Menu>
+
       <Image src={props.src} {...props} />
     </Box>
   )
