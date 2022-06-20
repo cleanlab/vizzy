6b2f92f568240aa7f37b08c91140fe36d028f96e
calebchiam <calebchiam@gmail.com>
2022-06-20 13:18:28 -0700

    refactor PredProbsTable as its own component

--- src/components/predProbs/PredProbs.tsx
+++ src/components/predProbs/PredProbs.tsx
@@ -17,24 +17,10 @@ import {
   Flex,
 } from '@chakra-ui/react'
 import { PredProbsEntryProps, PredProbsProps } from './types'
+import PredProbsTable from './PredProbsTable'
 
 const PredProbs = (props: PredProbsProps) => {
   const { data } = props
-  const renderRow = (datapoint: PredProbsEntryProps) => {
-    return (
-      <Tr>
-        <Td>
-          <HStack height="40px">
-            <Image boxSize="50px" src={datapoint.src} />
-            <Divider orientation="vertical" />
-          </HStack>
-        </Td>
-        {datapoint.probabilities.map((prob) => (
-          <Td isNumeric>{prob}</Td>
-        ))}
-      </Tr>
-    )
-  }
 
   return (
     <VStack width={'100%'} height={'100%'}>
@@ -43,33 +29,10 @@ const PredProbs = (props: PredProbsProps) => {
       </Heading>
       {!data && (
         <Flex width={'100%'} height={'100%'} justify={'center'} align={'center'}>
-          Nothing found.
+          Nothing computed.
         </Flex>
       )}
-      {data && (
-        <TableContainer overflowY={'auto'}>
-          <Table variant="simple" size="sm">
-            {/*<TableCaption placement="top">Predicted Probabilities for each label</TableCaption>*/}
-            <Thead>
-              <Tr>
-                <Th isNumeric>Example</Th>
-                <Th isNumeric>C1</Th>
-                <Th isNumeric>C2</Th>
-                <Th isNumeric>C3</Th>
-              </Tr>
-            </Thead>
-            <Tbody>{Object.values(data).map((datapoint) => renderRow(datapoint))}</Tbody>
-            <Tfoot>
-              <Tr>
-                <Th isNumeric>Example</Th>
-                <Th isNumeric>C1</Th>
-                <Th isNumeric>C2</Th>
-                <Th isNumeric>C3</Th>
-              </Tr>
-            </Tfoot>
-          </Table>
-        </TableContainer>
-      )}
+      {data && <PredProbsTable data={Object.values(data)} />}
     </VStack>
   )
 }
---
+++ src/components/predProbs/PredProbsTable.tsx
@@ -0,0 +1,64 @@
+import {
+  Divider,
+  HStack,
+  Image,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react'
+import React from 'react'
+import { PredProbsEntryProps } from './types'
+
+interface PredProbsTableProps {
+  data: Array<PredProbsEntryProps>
+}
+const PredProbsTable = (props: PredProbsTableProps) => {
+  const { data } = props
+  const renderRow = (datapoint: PredProbsEntryProps) => {
+    return (
+      <Tr>
+        <Td>
+          <HStack height="40px">
+            <Image boxSize="50px" src={datapoint.src} />
+            <Divider orientation="vertical" />
+          </HStack>
+        </Td>
+        {datapoint.probabilities.map((prob) => (
+          <Td isNumeric>{prob}</Td>
+        ))}
+      </Tr>
+    )
+  }
+
+  return (
+    <TableContainer overflowY={'auto'}>
+      <Table variant="simple" size="sm">
+        {/*<TableCaption placement="top">Predicted Probabilities for each label</TableCaption>*/}
+        <Thead>
+          <Tr>
+            <Th isNumeric>Example</Th>
+            <Th isNumeric>C1</Th>
+            <Th isNumeric>C2</Th>
+            <Th isNumeric>C3</Th>
+          </Tr>
+        </Thead>
+        <Tbody>{Object.values(data).map((datapoint) => renderRow(datapoint))}</Tbody>
+        <Tfoot>
+          <Tr>
+            <Th isNumeric>Example</Th>
+            <Th isNumeric>C1</Th>
+            <Th isNumeric>C2</Th>
+            <Th isNumeric>C3</Th>
+          </Tr>
+        </Tfoot>
+      </Table>
+    </TableContainer>
+  )
+}
+
+export default PredProbsTable
