060f0a17bb5a5626b5a2bdfcfb7c4e5048961585
calebchiam <calebchiam@gmail.com>
2022-08-09 21:12:50 +0200

    add links to personal githubs

--- src/components/misc/BuiltBy.tsx
+++ src/components/misc/BuiltBy.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import { Flex, Text } from '@chakra-ui/react'
+import { Flex, Link, Text } from '@chakra-ui/react'
 
 const BuiltBy = () => {
   return (
     <Flex w={'100%'} justify={'flex-end'}>
-      <Text fontSize={'md'}>Built with ♥ by Caleb, Luke, Yiming at Cleanlab</Text>
+      <Text fontSize={'md'}>
+        Built with ♥ by <Link href={'https://github.com/calebchiam'}>Caleb</Link>,{' '}
+        <Link href={'https://github.com/LukeMainwaring'}>Luke</Link>,{' '}
+        <Link href={'https://github.com/yimingc9'}>Yiming</Link> at Cleanlab
+      </Text>
     </Flex>
   )
 }
