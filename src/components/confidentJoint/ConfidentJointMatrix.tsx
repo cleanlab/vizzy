ff32a9ab8c4c5fc1252fcf02643b4b414fdfd45a
calebchiam <calebchiam@gmail.com>
2022-08-12 16:17:21 +0200

    rotate given label tag

--- src/components/confidentJoint/ConfidentJointMatrix.tsx
+++ src/components/confidentJoint/ConfidentJointMatrix.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Flex, HStack, Tag, VStack } from '@chakra-ui/react'
+import { Box, Flex, HStack, Tag, Text, VStack } from '@chakra-ui/react'
 import { LabelIssue } from '../issues/types'
 import ImageGrid from './ImageGrid'
 
@@ -12,14 +12,12 @@ interface ConfidentJointProps {
 
 const ConfidentJointMatrix = (props: ConfidentJointProps) => {
   const { labels, issues, activeImageId, setActiveImageId } = props
-  const gridLength = 'calc(max(575px, min(34vw, 640px)))'
-  const tagSizePercent = '4%'
-  const cellSizePercent = '32%'
+  const gridLength = 'calc(max(33.5rem, 32vw))'
+  const cellSizePercent = '33%'
 
   return (
-    <VStack spacing={0} h={gridLength}>
-      <HStack w={gridLength} h={tagSizePercent} spacing={0}>
-        <Box w={tagSizePercent}> </Box>
+    <VStack spacing={0} align={'flex-end'}>
+      <HStack w={gridLength} spacing={0}>
         {labels.map((label) => (
           <Flex key={label} w={cellSizePercent} justify={'center'} mb={'2px'}>
             <Tag colorScheme={'yellow'} size={'md'}>
@@ -28,54 +26,55 @@ const ConfidentJointMatrix = (props: ConfidentJointProps) => {
           </Flex>
         ))}
       </HStack>
-      {[0, 1, 2].map((rowIdx) => (
-        <HStack key={rowIdx} spacing={0} w={gridLength} h={cellSizePercent}>
-          <Flex
-            w={tagSizePercent}
-            justify={'center'}
-            align={'flex-end'}
-            transform={'rotate(270deg)'}
-            mr={'2px'}
-          >
-            <Tag colorScheme={'blue'} size={'md'} minWidth={'max-content'}>
-              {`given: ${labels[rowIdx]}`}
-            </Tag>
-          </Flex>
-          {[0, 1, 2].map((columnIdx) => {
-            const suggestedLabel = labels[columnIdx]
-            const givenLabel = labels[rowIdx]
-            const activeIssue = activeImageId && issues[activeImageId]
-            // if the active image ID doesn't fall within this grid, it doesn't
-            // need to know about it, improving memoization
-            const localActiveImageId =
-              activeImageId &&
-              activeIssue.givenLabel === givenLabel &&
-              activeIssue.suggestedLabel === suggestedLabel
-                ? activeImageId
-                : null
-            return (
-              <Box
-                key={columnIdx}
-                borderWidth={'0.5px'}
-                m={0}
-                borderColor={'gray.500'}
-                w={cellSizePercent}
-                h={'100%'}
-              >
-                {issues && (
-                  <ImageGrid
-                    suggestedLabel={suggestedLabel}
-                    givenLabel={givenLabel}
-                    activeImageId={localActiveImageId}
-                    setActiveImageId={setActiveImageId}
-                    issues={issues}
-                  />
-                )}
-              </Box>
-            )
-          })}
-        </HStack>
-      ))}
+      <VStack spacing={0} h={gridLength}>
+        {[0, 1, 2].map((rowIdx) => (
+          <HStack key={rowIdx} spacing={0} height={cellSizePercent}>
+            <Flex justify={'center'} align={'flex-end'} mr={'2px'}>
+              <Tag colorScheme={'blue'} size={'md'} minWidth={'max-content'}>
+                <VStack spacing={0}>
+                  <Text>given:</Text>
+                  <Text>{labels[rowIdx]}</Text>
+                </VStack>
+              </Tag>
+            </Flex>
+            <HStack spacing={0} w={gridLength} h={'100%'}>
+              {[0, 1, 2].map((columnIdx) => {
+                const suggestedLabel = labels[columnIdx]
+                const givenLabel = labels[rowIdx]
+                const activeIssue = activeImageId && issues[activeImageId]
+                // if the active image ID doesn't fall within this grid, it doesn't
+                // need to know about it, improving memoization
+                const localActiveImageId =
+                  activeImageId &&
+                  activeIssue.givenLabel === givenLabel &&
+                  activeIssue.suggestedLabel === suggestedLabel
+                    ? activeImageId
+                    : null
+                return (
+                  <Box
+                    key={columnIdx}
+                    borderWidth={'0.5px'}
+                    m={0}
+                    borderColor={'gray.500'}
+                    w={cellSizePercent}
+                    h={'100%'}
+                  >
+                    {issues && (
+                      <ImageGrid
+                        suggestedLabel={suggestedLabel}
+                        givenLabel={givenLabel}
+                        activeImageId={localActiveImageId}
+                        setActiveImageId={setActiveImageId}
+                        issues={issues}
+                      />
+                    )}
+                  </Box>
+                )
+              })}
+            </HStack>
+          </HStack>
+        ))}
+      </VStack>
     </VStack>
   )
 }
