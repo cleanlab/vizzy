0db5e3f09267a2366804e5989cd8e7178670f0bd
Luke Mainwaring <lukemain@sas.upenn.edu>
2022-08-13 03:58:13 -0400

    radio buttons for label selection and reorder example to show label error (#8)
    * radio buttons for label selection and reorder example
    
    * black font for selected label
    
    * dark border for selected label
    
    * border for non-selected and hover
    
    Co-authored-by: Luke Mainwaring <lukemainwaring@Lukes-MacBook-Pro.local>

--- src/components/dataset/ImageWithLabel/ImageWithLabel.tsx
+++ src/components/dataset/ImageWithLabel/ImageWithLabel.tsx
@@ -1,33 +1,47 @@
 import React from 'react'
-import { Box, Image, Select } from '@chakra-ui/react'
+import { Box, Image, RadioGroup, useRadioGroup, HStack } from '@chakra-ui/react'
+import { RadioCard } from './RadioCard'
 import { ImageWithLabelProps } from '../types'
 import './ImageWithLabel.css'
 import placeholder from '../../../assets/placeholder.png'
 
 const ImageWithLabel = (props: ImageWithLabelProps) => {
   const { id, givenLabel, classes, updateLabel, setActiveImageId, ...imageProps } = props
+
+  const handleChange = (value) => {
+    updateLabel(id, value)
+  }
+  const { getRootProps, getRadioProps } = useRadioGroup({
+    name: 'select-label',
+    defaultValue: givenLabel,
+    onChange: handleChange,
+  })
+
+  const group = getRootProps()
+
   return (
     <Box position={'relative'} onMouseEnter={() => setActiveImageId(id)}>
-      <Select
-        // colorScheme={useColorModeValue('blackAlpha', 'blackAlpha')}
-        backgroundColor={'white'}
-        opacity={'60%'}
+      <RadioGroup
+        backgroundColor={'rgba(255,255,255,0.6)'}
         color={'black'}
-        size={'sm'}
+        width={'100%'}
+        maxWidth={'100%'}
         bottom={'0px'}
         right={'0px'}
         position={'absolute'}
-        maxWidth={'100%'}
         overflowX={'auto'}
-        defaultValue={givenLabel}
-        onChange={(evt) => {
-          updateLabel(id, evt.target.value)
-        }}
       >
-        {classes.map((v) => (
-          <option key={v}>{v}</option>
-        ))}
-      </Select>
+        <HStack {...group} width={'100%'} maxWidth={'100%'} justifyContent="center">
+          {classes.map((value) => {
+            const radio = getRadioProps({ value })
+            return (
+              <RadioCard key={value} {...radio}>
+                {value}
+              </RadioCard>
+            )
+          })}
+        </HStack>
+      </RadioGroup>
 
       <Image {...imageProps} loading={'lazy'} fallbackSrc={placeholder} />
     </Box>
---
+++ src/components/dataset/ImageWithLabel/RadioCard.tsx
@@ -0,0 +1,43 @@
+import { Box, useRadio } from '@chakra-ui/react'
+import * as React from 'react'
+
+export const RadioCard = (props) => {
+  const { getInputProps, getCheckboxProps } = useRadio(props)
+
+  const input = getInputProps()
+  const checkbox = getCheckboxProps()
+
+  return (
+    <Box as="label">
+      <input {...input} />
+      <Box
+        {...checkbox}
+        backgroundColor={'gray.300'}
+        cursor="pointer"
+        borderRadius="md"
+        border="1px"
+        borderColor={'gray.500'}
+        color={'gray.600'}
+        boxShadow="lg"
+        _checked={{
+          bg: 'teal.400',
+          color: 'black',
+          borderColor: 'gray.800',
+        }}
+        _focus={{
+          boxShadow: 'outline',
+        }}
+        _hover={{
+          backgroundColor: 'gray.400',
+        }}
+        px={3}
+        py={0.5}
+        fontSize={'sm'}
+        marginTop={'1'}
+        marginBottom={'1'}
+      >
+        {props.children}
+      </Box>
+    </Box>
+  )
+}
