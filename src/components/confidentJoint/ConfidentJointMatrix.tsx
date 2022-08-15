4898d9c8d010d5b280938a46be038b96761ff381
calebchiam <calebchiam@gmail.com>
2022-08-15 20:55:44 +0200

    red.50 -> red.100
    red.50 looks too much like white

--- src/components/confidentJoint/ConfidentJointMatrix.tsx
+++ src/components/confidentJoint/ConfidentJointMatrix.tsx
@@ -14,7 +14,7 @@ const ConfidentJointMatrix = (props: ConfidentJointProps) => {
   const { labels, issues, activeImageId, setActiveImageId } = props
   const gridLength = 'calc(max(33.5rem, 34vw))'
   const cellSizePercent = '33%'
-  const errorCellColor = useColorModeValue('red.50', 'red.900')
+  const errorCellColor = useColorModeValue('red.100', 'red.900')
 
   return (
     <VStack spacing={0} align={'flex-end'}>
